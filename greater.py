4d17648bf17d63eaf7d1b5da1a11c5c4fe537456
Kaiden664 <kaidenarav@gmail.com>
2026-06-07 13:15:25 -0400

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,9 @@
+x = float(input("Enter your marks: "))
+if (x > 15):
+    print("you have passed the exam")
+    print("it is an if block")
+elif (x ==15):
+    print("you have passed the exam")
+else:
+    print("you have not passed the exam")
+    print("it is an else block")
